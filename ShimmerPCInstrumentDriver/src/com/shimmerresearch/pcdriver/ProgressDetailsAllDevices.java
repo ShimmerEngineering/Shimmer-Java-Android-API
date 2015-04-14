f99c144b286e74408ee3b14cdea11fd2243b2c25
Alejandro Saez <alejandrosaez3@gmail.com>
2015-04-14 09:08:27 +0100

    Update

--- ShimmerPCInstrumentDriver/src/com/shimmerresearch/pcdriver/ProgressDetailsAllDevices.java
+++ ShimmerPCInstrumentDriver/src/com/shimmerresearch/pcdriver/ProgressDetailsAllDevices.java
@@ -1,5 +1,13 @@
 package com.shimmerresearch.pcdriver;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProgressDetailsAllDevices {
 
 	
@@ -16,7 +24,33 @@ public class ProgressDetailsAllDevices {
 		}
 	}
 	
-	
+	//the key is the mac address + "." + session id
 	SerializableTreeMap<String , ProgressDetailsPerDevice> mMapOfDeviceProgressInfo = new SerializableTreeMap<String, ProgressDetailsPerDevice>();
 	
+	public int mNumberOfFails = 0;
+	public List<String> mListOfFailedDevices = new ArrayList<String>();
+	public int mProgressPercentageComplete = 0;
+	
+	
+	/**Performs a deep copy of ProgressDetailsAll by Serializing
+	 * @return ProgressDetailsAll the deep copy of the current ProgressDetailsAll
+	 * @see java.io.Serializable
+	 */
+	public ProgressDetailsAllSessions deepClone() {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(baos);
+			oos.writeObject(this);
+
+			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+			ObjectInputStream ois = new ObjectInputStream(bais);
+			return (ProgressDetailsAllSessions) ois.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
