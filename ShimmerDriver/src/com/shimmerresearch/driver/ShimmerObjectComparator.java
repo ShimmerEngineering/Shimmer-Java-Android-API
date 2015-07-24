474e6e90aea2dc2abf370edef338a89625d1f85c
RuMolloy <rmolloy10@gmail.com>
2015-07-24 16:41:36 +0100

    minor

--- ShimmerDriver/src/com/shimmerresearch/driver/ShimmerObjectComparator.java
+++ ShimmerDriver/src/com/shimmerresearch/driver/ShimmerObjectComparator.java
@@ -4,9 +4,9 @@ import java.util.Comparator;
 
 public class ShimmerObjectComparator implements Comparator<ShimmerObject> {
 	
-	public enum Order {LOCATION, BT_RADIO_ID, EXPANSION, NAME}
+	public enum OrderShimmerObject {LOCATION, BT_RADIO_ID, EXPANSION, FIRMWARE, BATTERY, NAME}
 	
-	private Order sortingBy = Order.LOCATION;
+	private OrderShimmerObject sortingBy = OrderShimmerObject.LOCATION;
 
 	@Override
 	public int compare(ShimmerObject shimmerObject1, ShimmerObject shimmerObject2) {
@@ -14,13 +14,19 @@ public class ShimmerObjectComparator implements Comparator<ShimmerObject> {
 			case LOCATION: return shimmerObject1.mUniqueID.compareTo(shimmerObject2.mUniqueID);
 			case BT_RADIO_ID: return shimmerObject1.getMacIdFromUartParsed().compareTo(shimmerObject2.getMacIdFromUartParsed());
 			case EXPANSION: return shimmerObject1.mExpansionBoardParsed.compareTo(shimmerObject2.mExpansionBoardParsed);
+			case FIRMWARE: return shimmerObject1.mFirmwareVersionParsed.compareTo(shimmerObject2.mFirmwareVersionParsed);
+			case BATTERY: return shimmerObject1.getEstimatedChargePercentage().compareTo(shimmerObject2.getEstimatedChargePercentage());
 			case NAME: return shimmerObject1.getShimmerUserAssignedName().compareTo(shimmerObject2.getShimmerUserAssignedName());
 			default: return 0;
 		}
 	}
 	
-	public void setSortingBy(Order sortBy) {
+	public void setSortingBy(OrderShimmerObject sortBy) {
 		this.sortingBy = sortBy;
+	}
+	
+	public OrderShimmerObject getSortingBy(){
+		return this.sortingBy;
 	}
 
 }
