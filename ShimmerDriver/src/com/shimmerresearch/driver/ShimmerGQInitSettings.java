3a4f1b1041545189539f246919e6cc7e5b8f08be
JongChern <jclim@shimmersensing.com>
2015-10-11 20:33:54 +0800

    GQ updates

---
+++ ShimmerDriver/src/com/shimmerresearch/driver/ShimmerGQInitSettings.java
@@ -0,0 +1,14 @@
+package com.shimmerresearch.driver;
+
+import java.util.HashMap;
+
+public class ShimmerGQInitSettings {
+
+	int mChannelID;
+	String mShimmerGQID;
+	/**String is the shimmer gq IDs 
+	 * 
+	 */
+	HashMap<String,ShimmerVerObject> mMapOfSpanIDtoSpanSVO;
+	
+}
