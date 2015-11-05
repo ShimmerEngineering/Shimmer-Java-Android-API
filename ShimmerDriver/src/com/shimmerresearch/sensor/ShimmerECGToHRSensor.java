399591968c975f29a360311c1c75499da7e055da
JongChern <jclim@shimmersensing.com>
2015-11-05 14:20:49 +0800

    minor updates

--- ShimmerDriver/src/com/shimmerresearch/sensor/ShimmerECGToHRSensor.java
+++ ShimmerDriver/src/com/shimmerresearch/sensor/ShimmerECGToHRSensor.java
@@ -3,19 +3,24 @@ package com.shimmerresearch.sensor;
 import java.util.HashMap;
 
 import com.shimmerresearch.driver.ChannelDetails;
-import com.shimmerresearch.driver.Configuration.COMMUNICATION_TYPE;
-import com.shimmerresearch.driver.Configuration.Shimmer3.CompatibilityInfoForMaps;
 import com.shimmerresearch.driver.Configuration;
+import com.shimmerresearch.driver.Configuration.COMMUNICATION_TYPE;
 import com.shimmerresearch.driver.SensorConfigOptionDetails;
-import com.shimmerresearch.driver.ShimmerVerDetails;
+import com.shimmerresearch.driver.Configuration.Shimmer3.CompatibilityInfoForMaps;
+import com.shimmerresearch.driver.ShimmerVerDetails.FW_ID;
 import com.shimmerresearch.driver.ShimmerVerObject;
 
-public class ShimmerGSRSensor extends AbstractSensor{
+public class ShimmerECGToHRSensor extends AbstractSensor{
 	
 	
-	
-	public ShimmerGSRSensor(ShimmerVerObject svo) {
+
+	public ShimmerECGToHRSensor(ShimmerVerObject svo) {
 		super(svo);
+		// TODO Auto-generated constructor stub
+		if (mFirmwareType == FW_ID.SHIMMER3.GQ_802154){
+			
+		} 
+		
 		
 	}
 
@@ -32,19 +37,16 @@ public class ShimmerGSRSensor extends AbstractSensor{
 	}
 
 
-
+	@Override
+	public Object processData(byte[] rawData, int FWType, int sensorFWID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
 	public ActionSetting setSettings(String componentName, Object valueToSet,
 			COMMUNICATION_TYPE comType) {
-		
 		// TODO Auto-generated method stub
-		if (comType == COMMUNICATION_TYPE.BLUETOOTH){
-			
-		} else if(comType == COMMUNICATION_TYPE.SD){
-			
-		}
-		
 		return null;
 	}
 
@@ -58,20 +60,6 @@ public class ShimmerGSRSensor extends AbstractSensor{
 	@Override
 	public HashMap<String, SensorConfigOptionDetails> generateConfigOptionsMap(
 			ShimmerVerObject svo) {
-		// TODO Auto-generated method stub
-		
-		
-				mConfigOptionsMap.put(Configuration.Shimmer3.GuiLabelConfig.GSR_RANGE, 
-						new SensorConfigOptionDetails(Configuration.Shimmer3.ListofGSRRange, 
-												Configuration.Shimmer3.ListofGSRRangeConfigValues, 
-												SensorConfigOptionDetails.GUI_COMPONENT_TYPE.COMBOBOX,
-												CompatibilityInfoForMaps.listOfCompatibleVersionInfoGsr));
-				
-		return null;
-	}
-
-	@Override
-	public Object processData(byte[] rawData, int FWType, int sensorFWID) {
 		// TODO Auto-generated method stub
 		return null;
 	}
