33172c0f38ed1c2f40dbbb6ab9f7c6be915ea815
marknolan <markinolan@gmail.com>
2016-03-02 09:36:11 +0000

    Working on GQ panel comparators.

--- ShimmerDriver/src/com/shimmerresearch/shimmerUartProtocol/ShimmerUart.java
+++ ShimmerDriver/src/com/shimmerresearch/shimmerUartProtocol/ShimmerUart.java
@@ -1,7 +1,6 @@
 package com.shimmerresearch.shimmerUartProtocol;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.nio.ByteBuffer;
