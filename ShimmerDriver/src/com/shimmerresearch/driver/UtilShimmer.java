f3925e0d542daa79fff424bd28825e804f852cf7
marknolan <markinolan@gmail.com>
2016-04-07 09:11:19 +0100

    Tidying ShimmerOsInfo and bug fix with Cadence.

--- ShimmerDriver/src/com/shimmerresearch/driver/UtilShimmer.java
+++ ShimmerDriver/src/com/shimmerresearch/driver/UtilShimmer.java
@@ -16,7 +16,6 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TimeZone;
