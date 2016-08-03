3df0e32a04276d673c39981a6a1395626343630f
Ronan Mc Cormack <Ronan.shimmersensing@gmail.com>
2016-08-03 16:52:52 +0100

    minor

---
+++ ShimmerDriver/src/com/shimmerresearch/driverUtilities/OldCalDetails.java
@@ -0,0 +1,14 @@
+package com.shimmerresearch.driverUtilities;
+
+public class OldCalDetails{
+	
+	public String mFileSearchString = "";
+	public int mSensorMapKey = 0;
+	public int mRange = 0;
+	
+	public OldCalDetails(String fileSearchString, int sensorMapKey, int range){
+		this.mFileSearchString = fileSearchString;
+		this.mSensorMapKey = sensorMapKey;
+		this.mRange = range;
+	}
+}
