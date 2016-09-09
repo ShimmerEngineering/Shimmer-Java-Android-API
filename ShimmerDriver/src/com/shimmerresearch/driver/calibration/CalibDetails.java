cb4aa071ca49763d8feafb98e6243f6d3f43c6fe
RuMolloy <rmolloy10@gmail.com>
2016-09-09 14:34:00 -0400

    added icons for calibration read source

--- ShimmerDriver/src/com/shimmerresearch/driver/calibration/CalibDetails.java
+++ ShimmerDriver/src/com/shimmerresearch/driver/calibration/CalibDetails.java
@@ -1,6 +1,9 @@
 package com.shimmerresearch.driver.calibration;
 
+import java.awt.Image;
 import java.io.Serializable;
+
+import javax.swing.ImageIcon;
 
 import org.apache.commons.lang3.ArrayUtils;
 
@@ -15,16 +18,35 @@ public abstract class CalibDetails implements Serializable {
 	public int mRangeValue = 0;
 	public long mCalibTimeMs = 0;
 	
+	private static ImageIcon imgCalib_Read_Source_Unknown = new ImageIcon(new ImageIcon(CalibDetails.class.getResource("/question grey.png")).getImage().getScaledInstance(16, 16,  java.awt.Image.SCALE_SMOOTH));
+	private static ImageIcon imgCalib_Read_SD_Header = new ImageIcon(new ImageIcon(CalibDetails.class.getResource("/sd grey.png")).getImage().getScaledInstance(10, 13,  java.awt.Image.SCALE_SMOOTH));
+	private static ImageIcon imgCalib_Read_Legacy_BT_Command = new ImageIcon(new ImageIcon(CalibDetails.class.getResource("/old_BT_command.png")).getImage().getScaledInstance(17, 17,  java.awt.Image.SCALE_SMOOTH));
+	private static ImageIcon imgCalib_Read_Infomem = new ImageIcon(new ImageIcon(CalibDetails.class.getResource("/memory_grey.png")).getImage().getScaledInstance(16, 16,  java.awt.Image.SCALE_SMOOTH));
+	private static ImageIcon imgCalib_Read_Radio_Dump = new ImageIcon(new ImageIcon(CalibDetails.class.getResource("/radio_dump.png")).getImage().getScaledInstance(16, 16,  java.awt.Image.SCALE_SMOOTH));
+	private static ImageIcon imgCalib_Read_File_Dump = new ImageIcon(new ImageIcon(CalibDetails.class.getResource("/file_dump.png")).getImage().getScaledInstance(16, 16,  java.awt.Image.SCALE_SMOOTH));
+	private static ImageIcon imgCalib_Read_User_Modified = new ImageIcon(new ImageIcon(CalibDetails.class.getResource("/user_grey.png")).getImage().getScaledInstance(18, 18,  java.awt.Image.SCALE_SMOOTH));
+	
 	//In order of increasing priority
 	public enum CALIB_READ_SOURCE{
-		UNKNOWN,
-		SD_HEADER,
-		LEGACY_BT_COMMAND,
-		INFOMEM,
-		RADIO_DUMP,
-		FILE_DUMP,
-		USER_MODIFIED
+		UNKNOWN(imgCalib_Read_Source_Unknown),
+		SD_HEADER(imgCalib_Read_SD_Header),
+		LEGACY_BT_COMMAND(imgCalib_Read_Legacy_BT_Command),
+		INFOMEM(imgCalib_Read_Infomem),
+		RADIO_DUMP(imgCalib_Read_Radio_Dump),
+		FILE_DUMP(imgCalib_Read_File_Dump),
+		USER_MODIFIED(imgCalib_Read_User_Modified);
+		
+	    private ImageIcon imgIcon = imgCalib_Read_Source_Unknown;
+		
+	    private CALIB_READ_SOURCE(final ImageIcon imgIcon) {
+	        this.imgIcon = imgIcon;
+	    }
+	    
+	    public ImageIcon getImage() {
+	        return this.imgIcon;
+	    }
 	}
+	
 	public CALIB_READ_SOURCE mCalibReadSource = CALIB_READ_SOURCE.UNKNOWN;
 
 	public abstract byte[] generateCalParamByteArray();
