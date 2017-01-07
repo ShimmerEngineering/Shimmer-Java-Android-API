5abf188608c927c482dc7e792497bf083f812d03
marknolan <markinolan@gmail.com>
2017-01-07 14:11:37 +0000

    Significant updates to Webcam devices and GUI Internal Frames

--- ShimmerDriver/src/main/java/com/shimmerresearch/driver/ShimmerMsg.java
+++ ShimmerDriver/src/main/java/com/shimmerresearch/driver/ShimmerMsg.java
@@ -3,9 +3,14 @@ package com.shimmerresearch.driver;
 public class ShimmerMsg {
 	public int mIdentifier;
 	public Object mB;
-	
-	public ShimmerMsg(int a, Object b){
+
+	public ShimmerMsg(int a) {
 		mIdentifier=a;
+	}
+
+	public ShimmerMsg(int a, Object b){
+		this(a);
 		mB=b;
 	}
+
 }
