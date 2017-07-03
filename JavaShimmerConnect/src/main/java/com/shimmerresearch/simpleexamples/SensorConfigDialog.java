4a4e4d898f90f255241bc97e4f3d8e6eac9834f1
jyong15 <jos15@yahoo.com>
2017-07-03 16:27:25 +0800

    JA-10
    Changed method of updating list with current settings to use the
    configvaluelabel in order to avoid conflicts as the index of the
    settings values in the Shimmer is non-linear

--- JavaShimmerConnect/src/main/java/com/shimmerresearch/simpleexamples/SensorConfigDialog.java
+++ JavaShimmerConnect/src/main/java/com/shimmerresearch/simpleexamples/SensorConfigDialog.java
@@ -2,6 +2,7 @@ package com.shimmerresearch.simpleexamples;
 
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -9,6 +10,8 @@ import javax.swing.JCheckBox;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +22,7 @@ import javax.swing.JTextField;
 import com.shimmerresearch.driverUtilities.ConfigOptionDetails;
 import com.shimmerresearch.driverUtilities.ConfigOptionDetailsSensor;
 import com.shimmerresearch.driverUtilities.SensorDetails;
+import com.shimmerresearch.exceptions.ShimmerException;
 import com.shimmerresearch.pcDriver.ShimmerPC;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -70,48 +74,94 @@ public class SensorConfigDialog {
 			ConfigOptionDetailsSensor cods = configOptionsMap.get(key);
 			String[] cs = cods.getGuiValues();
 			
-			//scrollPane.add(arg0)
+			System.out.println("For key: " + key + " configValue is: " + cloneDevice.getConfigValueUsingConfigLabel(key));
+			
 			Box box = Box.createVerticalBox();
 			JLabel label = new JLabel();
 			label.setText(key);
 			box.add(label);
 			
-			
-				
+			if(cs != null) {
+				int numOfCheckboxes = cs.length;
+				JCheckBox[] checkBox = new JCheckBox[numOfCheckboxes];
 				if(cods.mGuiComponentType == ConfigOptionDetails.GUI_COMPONENT_TYPE.COMBOBOX) {
-					if(cs != null) {
-						for(String text : cs) {
-							JCheckBox checkBox = new JCheckBox(text, false);
-							box.add(checkBox);
-						}					
+					
+					//int currentConfigIndex = (int) cloneDevice.getConfigValueUsingConfigLabel(key);
+					String currentConfigLabel = getConfigValueLabelFromConfigLabel(key);
+					
+					for(int i=0; i<numOfCheckboxes; i++) {
+						
+						if(cs[i].equals(currentConfigLabel)) {
+							checkBox[i] = new JCheckBox(cs[i], true);
+						} else {
+							checkBox[i] = new JCheckBox(cs[i], false);
+						}
+						
+						final int a = i;
+						checkBox[i].addActionListener(new ActionListener() {
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								
+								if(checkBox[a].isSelected()) {
+									cloneDevice.setConfigValueUsingConfigLabel(key, cods.mConfigValues[a]);
+									clearOtherCheckboxes(checkBox, a);
+								} else {
+									//The current config setting has been selected - maintain the state of the checkbox as true
+									checkBox[a].setSelected(true);
+								}
+								
+							}
+						});
+						
+						box.add(checkBox[i]);
+						
 					}
-				}				
-				else if(cods.mGuiComponentType == ConfigOptionDetails.GUI_COMPONENT_TYPE.TEXTFIELD) {
+				}
+			}
+				
+//				if(cods.mGuiComponentType == ConfigOptionDetails.GUI_COMPONENT_TYPE.COMBOBOX) {
+//					if(cs != null) {
+//						for(int i=0; i<numOfCheckboxes; i++) {
+//							checkBox[i] = new JCheckBox(cs[i], false);
+//							box.add(checkBox[i]);
+//						}					
+//					}
+//				}				
+//				else if(cods.mGuiComponentType == ConfigOptionDetails.GUI_COMPONENT_TYPE.TEXTFIELD) {
+				if(cods.mGuiComponentType == ConfigOptionDetails.GUI_COMPONENT_TYPE.TEXTFIELD) {
 					JPanel textPanel = new JPanel();
 					textPanel.setLayout((LayoutManager) new BoxLayout(textPanel, BoxLayout.X_AXIS));
-					//textPanel.setLayout((LayoutManager) new FlowLayout(FlowLayout.LEFT));
 					textPanel.setSize(50, 15);
 					
 					JTextField textField = new JTextField();
 					textField.setToolTipText("Values are in Hz");
-					//textField.setColumns(5);
+					textField.setText((String) cloneDevice.getConfigValueUsingConfigLabel(key));
 					
 					JButton saveTextButton = new JButton("set");
+					saveTextButton.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							
+							String text  = textField.getText();
+							if(text.matches("[0-9.]*")) {
+								cloneDevice.setConfigValueUsingConfigLabel(key, text);
+								System.out.println("The numerical value is: \n\n\n" + cloneDevice.getConfigValueUsingConfigLabel(key));
+							
+							} else{
+								JOptionPane.showMessageDialog(dialog, 
+										"Error! The value entered is not a numeric value", "Error", JOptionPane.ERROR_MESSAGE);
+							}
+						}
+					});
+
 					textPanel.add(textField);
 					textPanel.add(saveTextButton);
 					
-					//box.setLayout((LayoutManager) new FlowLayout(FlowLayout.LEFT));
 					box.add(textPanel);
 					box.setSize(10, 15);
-
-					
-					System.out.println("It has entered the TEXTFIELD>>>>>>>>>>>!!!!!!!!");
 				}
 
-			
 			panel.add(box);
 
-			System.out.println("CODS: " + cods.mGuiComponentType);
 		}
 		
 		System.out.println("TEXTFIELD: " + ConfigOptionDetails.GUI_COMPONENT_TYPE.TEXTFIELD + "\nCOMBOBOX: " + ConfigOptionDetails.GUI_COMPONENT_TYPE.COMBOBOX);
@@ -121,6 +171,32 @@ public class SensorConfigDialog {
 		
 	}
 	
+	private void clearOtherCheckboxes (JCheckBox[] checkBox, int selectedCheckbox) {
+		for(int i=0; i<checkBox.length; i++) {
+			if(i != selectedCheckbox) {
+				checkBox[i].setSelected(false);
+			}
+		}
+	}
+	
+    private String getConfigValueLabelFromConfigLabel(String label){
+        ConfigOptionDetailsSensor cods = cloneDevice.getConfigOptionsMap().get(label);
+        int currentConfigInt = (int) cloneDevice.getConfigValueUsingConfigLabel(label);
+        int index = -1;
+        Integer[] values = cods.getConfigValues();
+        String[] valueLabels = cods.getGuiValues();
+        for (int i=0;i<values.length;i++){
+            if (currentConfigInt==values[i]){
+                index=i;
+            }
+        }
+        if (index==-1){
+            System.out.println();
+            return "";
+        }
+        return valueLabels[index];
+    }
+
 	
 	
 	
