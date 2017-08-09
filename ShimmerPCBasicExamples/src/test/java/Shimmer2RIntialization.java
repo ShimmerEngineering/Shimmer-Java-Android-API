8fab0e660a2ebb6bbf4f2d9e77bdba6b17a218e4
Jong Chern <jongchern@gmail.com>
2017-08-09 22:15:11 +0800

    minor updates

--- ShimmerPCBasicExamples/src/test/java/Shimmer2RIntialization.java
+++ ShimmerPCBasicExamples/src/test/java/Shimmer2RIntialization.java
@@ -7,11 +7,11 @@ public class Shimmer2RIntialization {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String myName="";
-		double samplingRate=128;
+		double samplingRate=256;
 		int accelRange=3;
-		int gsrRange=4;
+		int gsrRange=1;
 		int setEnabledSensors=  ShimmerPC.SENSOR_ACCEL|ShimmerPC.SENSOR_MAG;
-		int magGain=2;
+		int magGain=0;
 		int orientation=0;
 		ShimmerPC shimmer = new ShimmerPC( myName,  samplingRate,  accelRange,  gsrRange,  setEnabledSensors, magGain,  orientation);
 		shimmer.connect("COM20", null);
