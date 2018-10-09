17fd0089a74d24d7dc5eb1c140dcff2b7b1d07f3
MAzalya <43565312+MAzalya@users.noreply.github.com>
2018-10-09 16:30:30 +0800

    Unit test for HW FW version, write and read samplingrate, start, stop, multiplestartstop streaming.

--- ShimmerDriverPC/src/test/java/com/shimmerresearch/pcDriver/API_00001_GeneralBluetoothShimmer2R_ShimmerPC.java
+++ ShimmerDriverPC/src/test/java/com/shimmerresearch/pcDriver/API_00001_GeneralBluetoothShimmer2R_ShimmerPC.java
@@ -1,23 +1,24 @@
 package com.shimmerresearch.pcDriver;
 
-import static org.junit.Assert.*;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import com.shimmerresearch.driverUtilities.ShimmerVerDetails.FW_ID;
+import com.shimmerresearch.driverUtilities.ShimmerVerDetails.HW_ID;
 import com.shimmerresearch.exceptions.ShimmerException;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class API_00001_GeneralBluetoothShimmer2R_ShimmerPC {
 
 	static ShimmerPC shimmer = new ShimmerPC("ShimmerDevice"); 
+	public static final int SHIMMER_2R=2; 
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		shimmer.connect("COM7",null);
+		shimmer.connect("COM14",null);
 		Thread.sleep(10000);
 	}
 
@@ -34,9 +35,87 @@ public class API_00001_GeneralBluetoothShimmer2R_ShimmerPC {
 			assert(false);
 		}
 	}
-
+	
 	@Test
-	public void testBDisconnect() {
+	public void testBGetHW() {
+		if (shimmer.getHardwareVersion()==HW_ID.SHIMMER_2R){
+			assert(true);
+		} else {
+			assert(false);
+		}
+	}
+	
+	@Test
+	public void testCGetFW() {
+		if (shimmer.getFirmwareIdentifier()==FW_ID.BTSTREAM){
+			assert(true);
+		} else {
+			assert(false);
+		}
+	}
+	
+	@Test
+	public void testDWriteReadSamplingRate() throws Exception {
+		shimmer.writeShimmerAndSensorsSamplingRate(128);
+		Thread.sleep(5000);
+		if (shimmer.getSamplingRateShimmer()==128) {
+			assert(true);
+		} else {
+			assert(false);
+		}
+	}
+	
+	@Test
+	public void testEStartStreaming() throws Exception {
+			shimmer.startStreaming();
+			Thread.sleep(5000);
+			if (shimmer.isStreaming()) {
+				assert(true);
+			} else {
+				assert(false);
+			}
+		}
+	
+	@Test
+	public void testFStopStreaming() throws Exception {
+			shimmer.stopStreaming();
+			Thread.sleep(5000);
+			if (shimmer.isStreaming()) {
+				assert(false);
+			} else {
+				assert(true);
+			}
+		}
+	
+	@Test
+	public void testGMultipleStartStopStreaming() throws Exception {
+		for (double i=1; i<8 ; i++) {
+			if (i%2!=0) {	//odd value of i will start streaming
+				shimmer.startStreaming();
+				Thread.sleep(3000);
+				if (shimmer.isStreaming()) {
+					assert(true);
+				} else {
+					assert(false);
+				}
+			}
+			
+			else		//even value of i will stop streaming
+			{
+				shimmer.stopStreaming();
+				Thread.sleep(3000);
+				if (shimmer.isStreaming()) {
+					assert(false);
+				} else {
+					assert(true);
+				}
+			}
+			}
+		}
+		
+	
+	@Test
+	public void testHDisconnect() {
 		try {
 			shimmer.disconnect();
 		} catch (ShimmerException e) {
@@ -50,7 +129,5 @@ public class API_00001_GeneralBluetoothShimmer2R_ShimmerPC {
 			assert(true);
 		}
 	}
-
 	  
-
 }
