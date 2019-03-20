b2fe0ea0294c7ddff2f8e2fa414a988ae4c48ce0
Jong Chern <jongchern@gmail.com>
2019-03-20 15:40:06 +0800

    minor edit

--- ShimmerDriver/src/main/java/com/shimmerresearch/driver/MsgDock.java
+++ ShimmerDriver/src/main/java/com/shimmerresearch/driver/MsgDock.java
@@ -154,14 +154,14 @@ public class MsgDock {
 	public final static int MSG_ID_APPLY_ZSCORE_RESULT_AGGREGATOR_SUCCESS = 501;
 	public final static int MSG_ID_APPLY_ZSCORE_RESULT_AGGREGATOR_FAILURE = 502;
 	
-	public final static int MSG_ID_PROCESS_ZSCORE_UPDATE = 600; //progress 0 - 100%
-	public final static int MSG_ID_PROCESS_ZSCORE_SUCCESS = 601;
-	public final static int MSG_ID_PROCESS_ZSCORE_FAILURE = 602;
-	public final static int MSG_ID_PROCESS_ZSCORE_CANCELLED = 603;
+	public final static int MSG_ID_PROCESS_ZSCORE_UPDATE = 510; //progress 0 - 100%
+	public final static int MSG_ID_PROCESS_ZSCORE_SUCCESS = 511;
+	public final static int MSG_ID_PROCESS_ZSCORE_FAILURE = 512;
+	public final static int MSG_ID_PROCESS_ZSCORE_CANCELLED = 513;
 	
-	public final static int MSG_ID_PROCESS_PEAKS_UPDATE = 700; //progress 0 - 100%
-	public final static int MSG_ID_PROCESS_PEAKS_SUCCESS = 701;
-	public final static int MSG_ID_PROCESS_PEAKS_FAILURE = 702;
+	public final static int MSG_ID_PROCESS_PEAKS_UPDATE = 520; //progress 0 - 100%
+	public final static int MSG_ID_PROCESS_PEAKS_SUCCESS = 521;
+	public final static int MSG_ID_PROCESS_PEAKS_FAILURE = 522;
 	
 	//ImportManager
 	public final static int MSG_ID_IMPORT_DB_PARSER_UPDATE = 130;
