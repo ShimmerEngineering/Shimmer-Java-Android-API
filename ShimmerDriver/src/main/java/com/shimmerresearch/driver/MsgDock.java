88f7622b382497168d5bb07d219c0adcca0196f4
MAzalya <43565312+MAzalya@users.noreply.github.com>
2019-03-25 12:24:10 +0800

    add MSG_ID_PROCESS_PEAKS_CANCELLED

--- ShimmerDriver/src/main/java/com/shimmerresearch/driver/MsgDock.java
+++ ShimmerDriver/src/main/java/com/shimmerresearch/driver/MsgDock.java
@@ -162,6 +162,8 @@ public class MsgDock {
 	public final static int MSG_ID_PROCESS_PEAKS_UPDATE = 520; //progress 0 - 100%
 	public final static int MSG_ID_PROCESS_PEAKS_SUCCESS = 521;
 	public final static int MSG_ID_PROCESS_PEAKS_FAILURE = 522;
+	public final static int MSG_ID_PROCESS_PEAKS_CANCELLED  = 523;
+
 	
 	//ImportManager
 	public final static int MSG_ID_IMPORT_DB_PARSER_UPDATE = 130;
