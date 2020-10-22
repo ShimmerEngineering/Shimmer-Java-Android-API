2415d7b0bbccba6aaed803bad4364b89af3af0b2
Jong Chern <jongchern@gmail.com>
2020-10-22 17:10:33 +0800

    update

--- ShimmerBluetoothManager/src/main/java/com/shimmerresearch/managers/bluetoothManager/ShimmerBluetoothManager.java
+++ ShimmerBluetoothManager/src/main/java/com/shimmerresearch/managers/bluetoothManager/ShimmerBluetoothManager.java
@@ -619,6 +619,19 @@ public abstract class ShimmerBluetoothManager{
 		return list;
 	}
 	
+	/**
+	 * @param friendlyName
+	 * @return comport if found, and null if not
+	 */
+	public String getComPortFromFriendlyName(String friendlyName) {
+		for (BluetoothDeviceDetails bdd:mMapOfParsedBtComPorts.values()) {
+			if (bdd.mFriendlyName.equals(friendlyName)) {
+				return bdd.mComPort;
+			}
+		}
+		return null;
+	}
+	
 	//TODO add support for Shimmer4
 	public List<String[]> getListOfSignalsFromDevices(String address){
 		List<String []> list = new ArrayList<String []>();
@@ -638,6 +651,10 @@ public abstract class ShimmerBluetoothManager{
 	//---------- GET Methods end -------------------------------
 	
 	
+	/**
+	 * @author User
+	 *
+	 */
 	protected class ConnectThread extends Thread{
 		
 		String comPort;
@@ -786,6 +803,7 @@ public abstract class ShimmerBluetoothManager{
 			return portDetails;
 		}
 
+		
 		protected void connectUnknownShimmer() throws ShimmerException {
 			printMessage("Connecting to new Shimmer with connection handle = " + (connectThroughComPort? comPort:bluetoothAddress));
 			
