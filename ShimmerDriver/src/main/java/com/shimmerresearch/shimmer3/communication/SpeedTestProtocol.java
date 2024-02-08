25789ba3fa9670d95342809508d2b59a4587521c
JongChern <jongchern@gmail.com>
2024-02-08 15:36:17 +0800

    Create SpeedTestProtocol.java

---
+++ ShimmerDriver/src/main/java/com/shimmerresearch/shimmer3/communication/SpeedTestProtocol.java
@@ -0,0 +1,16 @@
+package com.shimmerresearch.shimmer3.communication;
+
+import com.shimmerresearch.verisense.communication.AbstractByteCommunication;
+
+public class SpeedTestProtocol {
+	AbstractByteCommunication mByteCommunication;
+	public SpeedTestProtocol(AbstractByteCommunication byteComm) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
