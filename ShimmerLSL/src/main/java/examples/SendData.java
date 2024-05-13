3d4fdf978c05d54e9f570932c01df853716f2427
Harith J. <hjamadi@shimmersensing.com>
2024-05-13 16:07:28 +0800

    update to support multiple btcomport for the lsl data stream

--- ShimmerLSL/src/main/java/examples/SendData.java
+++ ShimmerLSL/src/main/java/examples/SendData.java
@@ -18,9 +18,10 @@ import com.shimmerresearch.exceptions.ShimmerException;
 public class SendData extends BasicProcessWithCallBack {
     
     static List<ShimmerDevice> shimmerDevices = new ArrayList<>();
-    static List<LSL.StreamOutlet>[] outlets = new ArrayList[3]; // for each sensor type
+    static List<LSL.StreamOutlet> outlets = new ArrayList<>(); // flat list for each sensor of each device
     static BasicShimmerBluetoothManagerPc[] btManagers = new BasicShimmerBluetoothManagerPc[2];
-    static String[] btComports = {"Com5", "Com7"}; //to use multiple shimmer device
+    static String[] btComports = {"Com5", "Com7"}; // to use multiple shimmer device
+    static String[] shimmerNames = {"Shimmer_96A9","Shimmer_6749"};
     static final double SAMPLE_RATE = 51.2;
     static final int NUM_CHANNELS = 3;
     
@@ -33,17 +34,14 @@ public class SendData extends BasicProcessWithCallBack {
 
     public static void main(String[] args) throws IOException, InterruptedException  {
         
-        NativeLibraryLoader.loadLibrary(); //to use the lib/liblsl64.dll
+        NativeLibraryLoader.loadLibrary(); // to use the lib/liblsl64.dll
         
         for (int i = 0; i < btManagers.length; i++) {
             btManagers[i] = new BasicShimmerBluetoothManagerPc();
-        }
-        
-        for (int i = 0; i < btManagers.length; i++) {
-            System.out.println("Creating new StreamInfo for Device " + (i+1) + "...");
-            for(int j = 0; j < 3; j++) { // outlet for each sensor
-            	String type = "";
-            	String streamName = "";
+            System.out.println("Creating StreamInfos for Device " + (i+1) + "...");
+            for (int j = 0; j < 3; j++) { // outlet for each sensor
+                String type = "";
+                String streamName = "";
                 if (j == 0) {
                     streamName += "_Accel";
                     type = "Accel";
@@ -57,16 +55,13 @@ public class SendData extends BasicProcessWithCallBack {
                 LSL.StreamInfo info = new LSL.StreamInfo("SendData_Device" + (i+1) + "_" + btComports[i] + streamName, type, NUM_CHANNELS, SAMPLE_RATE, LSL.ChannelFormat.float32, "test");
                 LSL.XMLElement chns = info.desc().append_child("channels");
                 String[] labels = j == 0 ? ACCEL_LABELS : (j == 1 ? GYRO_LABELS : MAG_LABELS);
-                for (int k = 0; k < labels.length; k++) {
+                for (int k = 0; k < NUM_CHANNELS; k++) {
                     chns.append_child("channel")
-                            .append_child_value("label", labels[k])
-                            //.append_child_value("unit", "microvolts")
-                            .append_child_value("type", type);
+                        .append_child_value("label", labels[k])
+                        .append_child_value("type", type);
                 }
-                System.out.println("Creating outlet for Device " + (i+1) + "...");
-                if(outlets[j] == null)
-                    outlets[j] = new ArrayList<>();
-                outlets[j].add(new LSL.StreamOutlet(info));
+                System.out.println("Creating outlet for Device " + (i+1) + " Sensor " + type + "...");
+                outlets.add(new LSL.StreamOutlet(info));
             }
         }
         
@@ -83,7 +78,6 @@ public class SendData extends BasicProcessWithCallBack {
     @Override
     protected void processMsgFromCallback(ShimmerMsg shimmerMSG) {
         int ind = shimmerMSG.mIdentifier;
-        
         Object object = (Object) shimmerMSG.mB;
         
         if (ind == ShimmerPC.MSG_IDENTIFIER_STATE_CHANGE) {
@@ -91,59 +85,52 @@ public class SendData extends BasicProcessWithCallBack {
             if (callbackObject.mState == BT_STATE.CONNECTED) {
                 for (int i = 0; i < btManagers.length; i++) {
                     ShimmerDevice shimmerDevice = btManagers[i].getShimmerDeviceBtConnected(btComports[i]);
-                    if(shimmerDevice != null) {
+                    if (shimmerDevice != null) {
                         System.out.println("Device " + (i+1) + " connected.");
                         shimmerDevices.add(shimmerDevice);
                     }
                 }
             }
-        }
-        else if (ind == ShimmerPC.MSG_IDENTIFIER_NOTIFICATION_MESSAGE) {
+        } else if (ind == ShimmerPC.MSG_IDENTIFIER_NOTIFICATION_MESSAGE) {
             CallbackObject callbackObject = (CallbackObject)object;
             int msg = callbackObject.mIndicator;
-            if (msg== ShimmerPC.NOTIFICATION_SHIMMER_FULLY_INITIALIZED){
+            if (msg == ShimmerPC.NOTIFICATION_SHIMMER_FULLY_INITIALIZED) {
                 try {
                     for (ShimmerDevice shimmerDevice : shimmerDevices) {
-                        shimmerDevice.startStreaming();
+                        shimmerDevice.startStreaming();                 
+                        System.out.println("Device " + shimmerDevice.getShimmerUserAssignedName() + " streamed.");
                     }
                 } catch (ShimmerException e) {
                     e.printStackTrace();
                 }
             }
+        } else if (ind == ShimmerPC.MSG_IDENTIFIER_DATA_PACKET) {
+            handleDataPacket(shimmerMSG);
         }
-        else if (ind == ShimmerPC.MSG_IDENTIFIER_DATA_PACKET) {
-            System.out.println("Shimmer MSG_IDENTIFIER_DATA_PACKET");
-            ObjectCluster objc = (ObjectCluster) shimmerMSG.mB;
-            double accelX = objc.getFormatClusterValue("Accel_LN_X", "CAL");
-            double accelY = objc.getFormatClusterValue("Accel_LN_Y", "CAL");
-            double accelZ = objc.getFormatClusterValue("Accel_LN_Z", "CAL");
-            double gyroX = objc.getFormatClusterValue("Gyro_X", "CAL");
-            double gyroY = objc.getFormatClusterValue("Gyro_Y", "CAL");
-            double gyroZ = objc.getFormatClusterValue("Gyro_Z", "CAL");
-            double magX = objc.getFormatClusterValue("Mag_X", "CAL");
-            double magY = objc.getFormatClusterValue("Mag_Y", "CAL");
-            double magZ = objc.getFormatClusterValue("Mag_Z", "CAL");
-
-            double timestamp = objc.mSystemTimeStamp;
-
-            float[] accelData = {(float)accelX, (float)accelY, (float)accelZ};
-            float[] gyroData = {(float)gyroX, (float)gyroY, (float)gyroZ};
-            float[] magData = {(float)magX, (float)magY, (float)magZ};
-
-            if(!Double.isNaN(accelX) && !Double.isNaN(accelY) && !Double.isNaN(accelZ)) {
-                for (LSL.StreamOutlet outlet : outlets[0]) {
-                    outlet.push_sample(accelData, timestamp);
-                }
-            }
-            if(!Double.isNaN(gyroX) && !Double.isNaN(gyroY) && !Double.isNaN(gyroZ)) {
-                for (LSL.StreamOutlet outlet : outlets[1]) {
-                    outlet.push_sample(gyroData, timestamp);
-                }
-            }
-            if(!Double.isNaN(magX) && !Double.isNaN(magY) && !Double.isNaN(magZ)) {
-                for (LSL.StreamOutlet outlet : outlets[2]) {
-                    outlet.push_sample(magData, timestamp);
-                }
+    }
+    
+    private void handleDataPacket(ShimmerMsg shimmerMSG) {
+        ObjectCluster objc = (ObjectCluster) shimmerMSG.mB;
+        int deviceIndex = objc.getShimmerName().equals(shimmerNames[0]) ? 0 : 1;
+        
+        double[] sensorValues = {
+            objc.getFormatClusterValue("Accel_LN_X", "CAL"),
+            objc.getFormatClusterValue("Accel_LN_Y", "CAL"),
+            objc.getFormatClusterValue("Accel_LN_Z", "CAL"),
+            objc.getFormatClusterValue("Gyro_X", "CAL"),
+            objc.getFormatClusterValue("Gyro_Y", "CAL"),
+            objc.getFormatClusterValue("Gyro_Z", "CAL"),
+            objc.getFormatClusterValue("Mag_X", "CAL"),
+            objc.getFormatClusterValue("Mag_Y", "CAL"),
+            objc.getFormatClusterValue("Mag_Z", "CAL")
+        };
+        
+        for (int j = 0; j < 3; j++) {
+            int outletIndex = deviceIndex * 3 + j;
+            if (!Double.isNaN(sensorValues[j*3]) && !Double.isNaN(sensorValues[j*3 + 1]) && !Double.isNaN(sensorValues[j*3 + 2])) {
+                float[] data = {(float)sensorValues[j*3], (float)sensorValues[j*3 + 1], (float)sensorValues[j*3 + 2]};
+                outlets.get(outletIndex).push_sample(data);
+                System.out.println("Device " + (deviceIndex + 1) + " " + outlets.get(outletIndex).info().type() + " data sent.");
             }
         }
     }
