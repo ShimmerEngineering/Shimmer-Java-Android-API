b1299c6d3a4bf6c35235c3aa992c611fe71968ec
JongChern <jongchern@gmail.com>
2024-10-14 18:48:16 +0800

    connect workng now
    need to implement unit test for 3R now

---
+++ ShimmerDriverPC/src/test/java/com/shimmerresearch/shimmer3/communication/API_0000X_ByteCommunication3R.java
@@ -0,0 +1,80 @@
+package com.shimmerresearch.shimmer3.communication;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.shimmerresearch.bluetooth.ShimmerBluetooth.BT_STATE;
+import com.shimmerresearch.driver.BasicProcessWithCallBack;
+import com.shimmerresearch.driver.CallbackObject;
+import com.shimmerresearch.driver.ShimmerMsg;
+import com.shimmerresearch.exceptions.ShimmerException;
+import com.shimmerresearch.pcDriver.ShimmerPC;
+
+import bolts.TaskCompletionSource;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) // Test methods will be run in alphabetical order
+public class API_0000X_ByteCommunication3R extends BasicProcessWithCallBack{
+	ShimmerPC mDevice;
+	TaskCompletionSource<Boolean> mCalibrationTask;
+	@Before
+    public void setUp() {
+		mDevice = new ShimmerPC("COM99");
+		mDevice.setTestRadio(new ByteCommunicationSimulatorS3R("COM99"));
+		setWaitForData(mDevice);
+    }
+    
+    @Test
+    public void test001_testConnectandDisconnect() {
+    	mCalibrationTask = new TaskCompletionSource<Boolean>();
+    	mDevice.connect("","");
+    	
+    		mCalibrationTask = new TaskCompletionSource<>();
+    		try {
+				boolean result = mCalibrationTask.getTask().waitForCompletion(5, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+    	if (!mDevice.isConnected()) {
+    		assert(false);
+    	}
+    	if (!mDevice.getFirmwareVersionParsed().equals("LogAndStream v0.16.9")) {
+    		assert(false);
+    	}
+    	/*
+    	System.out.println(mDevice.getHardwareVersionParsed());
+    	if (!mDevice.getHardwareVersionParsed().equals("")) {
+    		assert(false);
+    	}
+    	*/
+    }
+
+	@Override
+	protected void processMsgFromCallback(ShimmerMsg shimmerMSG) {
+		// TODO Auto-generated method stub
+		  int ind = shimmerMSG.mIdentifier;
+
+		  Object object = (Object) shimmerMSG.mB;
+
+		if (ind == ShimmerPC.MSG_IDENTIFIER_STATE_CHANGE) {
+			if (ind == ShimmerPC.MSG_IDENTIFIER_STATE_CHANGE) {
+				CallbackObject callbackObject = (CallbackObject)object;
+				
+				if (callbackObject.mState == BT_STATE.CONNECTED) {
+					if (mCalibrationTask!=null) {
+						mCalibrationTask.setResult(true);
+						mCalibrationTask = null;
+					}
+				}
+		}
+		
+		}
+	}
+   
+}
