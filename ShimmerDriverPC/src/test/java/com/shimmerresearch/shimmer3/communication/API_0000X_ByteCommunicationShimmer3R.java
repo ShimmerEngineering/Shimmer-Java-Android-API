fc02cab5fc9ae6d15493999d1aed4996926c0ec8
Mas Azalya <masazalya@shimmersensing.com>
2024-10-23 12:16:07 +0800

    minor restructure

--- ShimmerDriverPC/src/test/java/com/shimmerresearch/shimmer3/communication/API_0000X_ByteCommunicationShimmer3R.java
+++ ShimmerDriverPC/src/test/java/com/shimmerresearch/shimmer3/communication/API_0000X_ByteCommunicationShimmer3R.java
@@ -102,46 +102,26 @@ public class API_0000X_ByteCommunicationShimmer3R extends BasicProcessWithCallBa
     	if (!mDevice.isConnected()) {
     		assert(false);
     	}
-		CalibDetailsBmp390 calibDetailsBmp390 = new CalibDetailsBmp390();
-		byte[] pressureResoResTest = {
-			    (byte) 0xE7, (byte) 0x6B, (byte) 0xF0, (byte) 0x4A, (byte) 0xF9, 
-			    (byte) 0xAB, (byte) 0x1C, (byte) 0x9B, (byte) 0x15, (byte) 0x06, 
-			    (byte) 0x01, (byte) 0xD2, (byte) 0x49, (byte) 0x18, (byte) 0x5F, 
-			    (byte) 0x03, (byte) 0xFA, (byte) 0x3A, (byte) 0x0F, (byte) 0x07, 
-			    (byte) 0xF5
-			};
-		
-		mDevice.mSensorBMPX80.parseCalParamByteArray(pressureResoResTest,CALIB_READ_SOURCE.INFOMEM);
-    	byte[] newPacket = {-68, 19, 112, -53, 7, 9, 8, -66, 4, 24, -5, 7, 2, -16, -70, (byte) 0x00, (byte) 0xCF, (byte) 0x7F ,(byte) 0x00, (byte) 0x17, (byte) 0x64, -128, -70, -75, 80, 4, -87, 40, -128, 127, -1, -1, -3, 80, 71};
-
-    	String[] dataType = {"u24", "i16", "i16", "i16", "i16", "i16", "i16", "u24", "u24", "u8", "i24r", "i24r", "u8", "i24r", "i24r", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null};
     	
-    	//PARSE DATA
-    	long[] newPacketInt = null;
-
+    	//BMP390 data parsing
     	try{
-    		newPacketInt = UtilParseData.parseData(newPacket, dataType);	//uncalib
-    		double UT = (double)newPacketInt[7];
-			double UP = (double)newPacketInt[8];
+    		mDevice.mSensorBMPX80.parseCalParamByteArray(mByteCommunicationSimulatorS3R.getPressureResoTest(),CALIB_READ_SOURCE.INFOMEM);
+    		long[] dataPacket = UtilParseData.parseData(mByteCommunicationSimulatorS3R.getTestDataPacket(), mByteCommunicationSimulatorS3R.getTestDataType());	//uncalib
+    		double UT = (double)dataPacket[7];
+			double UP = (double)dataPacket[8];
 			
 			double[] bmp390caldata = mDevice.mSensorBMPX80.calibratePressureSensorData(UP, UT);
-			double P = bmp390caldata[0];
-			double T = bmp390caldata[1];
+			double pressure = bmp390caldata[0];
+			double temperature = bmp390caldata[1];
 			
-			if(Math.round(P * 10000d) / 10000d != 100912.8176) {	//4d.p. accuracy
+			if(Math.round(pressure * 10000d) / 10000d != 100912.8176) {	//4d.p. accuracy
 	    		assert(false);
 			}
 			
-			if(Math.round(T * 10000d) / 10000d != 23.2659) {		//4d.p. accuracy
+			if(Math.round(temperature * 10000d) / 10000d != 23.2659) {		//4d.p. accuracy
 	    		assert(false);
 			}
     	} catch(IndexOutOfBoundsException e){
-    		String debugString = "SignalDataTypeArray:";
-    		for(String s:dataType){
-    			debugString += "\t" + s;
-    		}
-    		System.out.println(debugString);
-    		System.out.println("newPacket:\t" + UtilShimmer.bytesToHexStringWithSpacesFormatted(newPacket));
     		throw(e);
     	}
 	}
--- ShimmerDriverPC/src/test/java/com/shimmerresearch/shimmer3/communication/ByteCommunicationSimulatorS3R.java
+++ ShimmerDriverPC/src/test/java/com/shimmerresearch/shimmer3/communication/ByteCommunicationSimulatorS3R.java
@@ -35,4 +35,29 @@ public class ByteCommunicationSimulatorS3R extends ByteCommunicationSimulatorS3{
 		mBuffer.add((byte) 0x00);
 		mBuffer.add((byte) 0x01);
 	}
+	
+	//BMP390 
+	
+	public byte[] getPressureResoTest() {
+		byte[] pressureResoResTest = {
+			    (byte) 0xE7, (byte) 0x6B, (byte) 0xF0, (byte) 0x4A, (byte) 0xF9, 
+			    (byte) 0xAB, (byte) 0x1C, (byte) 0x9B, (byte) 0x15, (byte) 0x06, 
+			    (byte) 0x01, (byte) 0xD2, (byte) 0x49, (byte) 0x18, (byte) 0x5F, 
+			    (byte) 0x03, (byte) 0xFA, (byte) 0x3A, (byte) 0x0F, (byte) 0x07, 
+			    (byte) 0xF5
+			};
+		
+		return pressureResoResTest;
+	}
+	
+	public byte[] getTestDataPacket() {
+    	byte[] newPacket = {-68, 19, 112, -53, 7, 9, 8, -66, 4, 24, -5, 7, 2, -16, -70, (byte) 0x00, (byte) 0xCF, (byte) 0x7F ,(byte) 0x00, (byte) 0x17, (byte) 0x64, -128, -70, -75, 80, 4, -87, 40, -128, 127, -1, -1, -3, 80, 71};
+    	return newPacket;
+	}
+	
+	public String[] getTestDataType() {
+    	String[] dataType = {"u24", "i16", "i16", "i16", "i16", "i16", "i16", "u24", "u24", "u8", "i24r", "i24r", "u8", "i24r", "i24r", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null};
+    	return dataType;
+	}
+
 }
